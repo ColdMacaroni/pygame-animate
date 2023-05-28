64c3026177ab5e24bf320dc67caa4be0606de5ce
Sofa <squishysparrow7.7@gmail.com>
2023-05-29 01:51:21 +1200

    create and delete frame, move between frames

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ size = (600, 480)
 def update_surface(
     surf: pygame.Surface,
     stack: Iterable[pygame.Surface],
+    last: pygame.Surface,
     bg: None | pygame.Color,
 ):
     if bg is not None:
@@ -16,6 +17,9 @@ def update_surface(
 
     for s in stack:
         surf.blit(s, (0, 0))
+
+    # The last frame, above all
+    surf.blit(last, (0,0))
 
 
 def main():
@@ -27,14 +31,17 @@ def main():
     clock = pygame.time.Clock()
 
     # The surface that'll be drawn on the screen
-    surf = pygame.Surface(size).convert_alpha()
+    surf = pygame.Surface(size)
 
     # Hold all the frames
     app = App(size)
 
     # Background color of the thing
     bg = pygame.Color(0xFF, 0xFF, 0xFF)
-    update_surface(surf, app.get_frame_stack(4), bg)
+    update_surface(surf, [], app.frame.surface, bg)
+
+    onion_layers = 3
+    onion_stack = []
 
     # Determines whether the mouse is being dragged and thus is drawing
     dragging = False
@@ -64,7 +71,28 @@ def main():
                 case pygame.MOUSEMOTION:
                     if dragging:
                         app.draw(e.pos)
-                        update_surface(surf, app.get_frame_stack(4), bg)
+                        update_surface(surf, onion_stack, app.frame.surface, bg)
+
+                case pygame.KEYUP:
+                    match e.key:
+                        case pygame.K_PERIOD:
+                            app.next_frame()
+                            onion_stack = app.get_onion_stack(onion_layers)
+                            update_surface(surf, onion_stack, app.frame.surface, bg)
+
+                        case pygame.K_COMMA:
+                            app.prev_frame()
+                            onion_stack = app.get_onion_stack(onion_layers)
+                            update_surface(surf, onion_stack, app.frame.surface, bg)
+
+                        case pygame.K_n:
+                            app.new_frame()
+                            onion_stack = app.get_onion_stack(onion_layers)
+                            update_surface(surf, onion_stack, app.frame.surface, bg)
+
+                        case pygame.K_d:
+                            if len(app.frames) > 1:
+                                app.del_frame()
 
         screen.blit(surf, (0, 0))
 
