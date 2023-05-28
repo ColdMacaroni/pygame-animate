b761d6ae53046eadd98185c34f6246843011a41f
Sofa <squishysparrow7.7@gmail.com>
2023-05-29 00:28:45 +1200

    Canvas class for holding a frame and drawing on

---
+++ canvas.py
@@ -0,0 +1,71 @@
+from typing import Self, Sequence
+import pygame
+from enum import Enum, auto
+from pygame import Color, Surface
+
+
+class BrushSize(Enum):
+    SMALL = auto()
+    MEDIUM = auto()
+    BIG = auto()
+
+    def size(self) -> int:
+        """Returns the radius of the brush"""
+        match self:
+            case self.SMALL:
+                return 1
+            case self.MEDIUM:
+                return 2
+
+            case self.BIG:
+                return 4
+
+    @property
+    def order(self) -> Sequence[Self]:
+        """Brushes ordered from smalles to biggest"""
+        return (self.SMALL, self.MEDIUM, self.BIG)
+
+    def next(self):
+        """Gets the next bigger size"""
+        return self.order[(self.order.index(self) + 1) % len(self.order)]
+
+    def prev(self):
+        """Gets the next smaller size"""
+        return self.order[(self.order.index(self) - 1) % len(self.order)]
+
+
+class Canvas:
+    def __init__(
+        self, size: tuple[int, int], colorkey: Color = Color(0xFF, 0xFF, 0xFF)
+    ) -> None:
+        self.width, self.height = size
+        self.colorkey = colorkey
+
+        self.surface = Surface(size)
+        self.surface.set_colorkey(colorkey)
+
+    def draw(
+        self,
+        pos,
+        brush_size: BrushSize,
+        color: Color = Color(0x00, 0x00, 0x00),
+    ):
+        """Draws a circle at the given position, with all the stuff"""
+        pygame.draw.circle(self.surface, color, pos, brush_size.size())
+
+    def save(self, fn: str):
+        """Save the surface to the given file"""
+        pygame.image.save(self.surface, fn)
+
+    def get_onion(self, color: Color, trans: int = 255) -> Surface:
+        """Returns a surface to be blitted as an onion layer. Unsure of the actual term"""
+        surf = self.surface.copy()
+
+        # Replace pixels that have been drawn on with the given colour.
+        for x in range(self.width):
+            for y in range(self.height):
+                if surf.get_at((x, y)) != self.colorkey:
+                    surf.set_at((x, y), color)
+
+        surf.set_alpha(trans)
+        return surf
